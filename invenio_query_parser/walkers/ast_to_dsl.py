8f9d8dc34d430324c1035a26b8257f0a92125900
Marios Kogias <marioskogias@gmail.com>
2014-11-19 13:43:26 +0100

    Start implementing elasticsearch dsl parser

---
+++ invenio_query_parser/walkers/ast_to_dsl.py
@@ -0,0 +1,50 @@
+"""Implement an invenion query AST to elasticsearch dsl converter"""
+
+from .. import ast
+from ..visitor import make_visitor
+
+
+class ASTtoDSLConverter(object):
+    visitor = make_visitor()
+
+    # not analyzed fields should be used as filters
+    #NOT_ANALYZED_FIELDS = set(["baz","boo"])
+
+    @visitor(ast.KeywordOp)
+    def visit(self, node, keyword, value):
+        """This function should return either a filter or a query
+           For now on it returns only queries
+        """
+        return value(keyword.value)
+
+    @visitor(ast.AndOp)
+    def visit(self, node, left, right):
+        return {"bool" : {"must": [left, right]}}
+
+    @visitor(ast.OrOp)
+    def visit(self, node, left, right):
+        return {"bool" : {"should": [left, right]}}
+
+    @visitor(ast.NotOp)
+    def visit(self, node, child):
+        return {"bool" : {"must_not": [child]}}
+
+    @visitor(ast.ValueQuery)
+    def visit(self, node, child):
+        return child("_all")
+
+    @visitor(ast.Keyword)
+    def visit(self, node):
+        return node
+
+    @visitor(ast.Value)
+    def visit(self, node):
+        return lambda x : {"match": { str(x): str(node.value)}}
+
+    @visitor(ast.SingleQuotedValue)
+    def visit(self, node):
+        return lambda x : {"match_phrase": { str(x): str(node.value)}}
+
+    @visitor(ast.DoubleQuotedValue)
+    def visit(self, node):
+        return lambda x : {"match": { str(x): str(node.value)}}
