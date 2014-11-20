785a964f2f93a609bd5e01c854f29d108c48893b
Marios Kogias <marioskogias@gmail.com>
2014-11-20 10:56:06 +0100

    Add multifield support

--- invenio_query_parser/walkers/ast_to_dsl.py
+++ invenio_query_parser/walkers/ast_to_dsl.py
@@ -10,24 +10,29 @@ class ASTtoDSLConverter(object):
     # not analyzed fields should be used as filters
     #NOT_ANALYZED_FIELDS = set(["baz","boo"])
 
+    def map_keyword_to_fields(self, keyword):
+        return [str(keyword)]
+
     @visitor(ast.KeywordOp)
     def visit(self, node, keyword, value):
         """This function should return either a filter or a query
            For now on it returns only queries
         """
-        return value(keyword.value)
+        l = self.map_keyword_to_fields(keyword.value)
+        print l
+        return value(l)
 
     @visitor(ast.AndOp)
     def visit(self, node, left, right):
-        return {"bool" : {"must": [left, right]}}
+        return {"bool": {"must": [left, right]}}
 
     @visitor(ast.OrOp)
     def visit(self, node, left, right):
-        return {"bool" : {"should": [left, right]}}
+        return {"bool": {"should": [left, right]}}
 
     @visitor(ast.NotOp)
     def visit(self, node, child):
-        return {"bool" : {"must_not": [child]}}
+        return {"bool": {"must_not": [child]}}
 
     @visitor(ast.ValueQuery)
     def visit(self, node, child):
@@ -39,17 +44,33 @@ class ASTtoDSLConverter(object):
 
     @visitor(ast.Value)
     def visit(self, node):
-        return lambda x : {"match": { str(x): str(node.value)}}
+        return lambda x: {
+            "multi_match": {
+                "query": str(node.value),
+                "fields": x
+            }
+            }
 
     @visitor(ast.SingleQuotedValue)
     def visit(self, node):
-        return lambda x : {"match_phrase": { str(x): str(node.value)}}
+        return lambda x: {
+            "multi_match": {
+                "query": str(node.value),
+                "type": "phrase",
+                "fields": x
+                }
+            }
 
     @visitor(ast.DoubleQuotedValue)
     def visit(self, node):
-        return lambda x : {"match": { str(x): str(node.value)}}
+        return lambda x: {
+            "bool": {
+                "should": [{"term": {str(k): str(node.value)}}
+                           for k in x]
+                }
+            }
 
     @visitor(ast.RangeOp)
     def visit(self, node, left, right):
-        return lambda x : {"range": { str(x): {"gte": node.left.value, "lte": node.right.value}}}
-
+        return lambda x: {"range": {str(x): {"gte": node.left.value,
+                                             "lte": node.right.value}}}
